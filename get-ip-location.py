f7938baf6845c3f7b94a813016d0172e770b46e3
3xp10it <3xp10it@users.noreply.github.com>
2016-07-29 09:19:16 +0800

    get the location of ip

---
+++ get-ip-location.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+#必须加上上面四行,否则各种编码的错误爆出
+import urllib2
+
+def getip():
+    return "113.116.175.200"
+url="http://ip.taobao.com/service/getIpInfo.php?ip=%s" % (getip())
+response = urllib2.urlopen(url)
+html = response.read()
+import json
+json_html=json.loads(html)
+print json_html['data']['country']
+print json_html['data']['city']
+print json_html['data']['region']
+
