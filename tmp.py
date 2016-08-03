4c7542bd2e476d8953277442810dac127ccba1c6
3xp10it <quanyechavshuo@gmail.com>
2016-08-04 07:26:03 +0800

    sjdfkl

---
+++ tmp.py
@@ -0,0 +1,21 @@
+#############################################################
+### +-+-+-+-+-+-+-+     
+### |3|x|p|1|0|i|t|     
+### +-+-+-+-+-+-+-+     
+###                                                          
+### name: tmp.py                                                  
+### function: sjdfkl                                         
+### date: 2016-08-04                                         
+### author: quanyechavshuo                                   
+### blog: https://3xp10it.github.io                          
+#############################################################
+# -*- coding: utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+#upon 4 lines for chinese support
+import time
+from exp10it import *
+figlet2file("3xp10it",2,True)
+time.sleep(1)
+
