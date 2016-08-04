c10a31e0fe824da55dfaa0ca1ce03a1a438e145a
3xp10it <quanyechavshuo@gmail.com>
2016-08-04 23:18:58 +0800

    slfdkj

---
+++ tmp2.py
@@ -0,0 +1,26 @@
+#############################################################
+###                                                           
+### _|_|_|                          _|    _|    _|    _|      
+###       _|  _|    _|  _|_|_|    _|_|  _|  _|      _|_|_|_|  
+###   _|_|      _|_|    _|    _|    _|  _|  _|  _|    _|      
+###       _|  _|    _|  _|    _|    _|  _|  _|  _|    _|      
+### _|_|_|    _|    _|  _|_|_|      _|    _|    _|      _|_|  
+###                     _|                                    
+###                     _|                                    
+###                                                          
+### name: tmp2.py
+### function: slfdkj
+### date: 2016-08-04
+### author: quanyechavshuo
+### blog: https://3xp10it.github.io
+#############################################################
+# -*- coding: utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+#upon 4 lines for chinese support
+import time
+from exp10it import *
+figlet2file("3xp10it","/tmp/figletpic",True)
+time.sleep(1)
+!!!!!!!!!
