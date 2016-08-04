95db0baea2e6039dc1144913995625d072e09767
3xp10it <quanyechavshuo@gmail.com>
2016-08-04 09:04:12 +0800

    haode,zheci

--- tmp.py
+++ tmp.py
@@ -1,13 +1,8 @@
 #############################################################
-### 33333 X   X PPPP   1   000  III TTTTT    
-###    3   X X  P   P 11  0   0  I    T      
-###   33    X   PPPP   1  0 0 0  I    T      
-### 3   3  X X  P      1  0   0  I    T      
-###  333  X   X P     111  000  III   T      
-###                                          
+### 3xp10it             
 ###                                                          
 ### name: tmp.py                                                  
-### function: sdjfkl                                         
+### function: haode,zheci                                    
 ### date: 2016-08-04                                         
 ### author: quanyechavshuo                                   
 ### blog: https://3xp10it.github.io                          
@@ -22,3 +17,4 @@ from exp10it import *
 figlet2file("3xp10it",2,True)
 time.sleep(1)
 
+this is a test
