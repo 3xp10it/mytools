06d9251bc16ade2e1426fc703e09e5f4370a6da8
3xp10it <quanyechavshuo@gmail.com>
2016-08-04 23:09:34 +0800

    sfdjlk

---
+++ tmp8.py
@@ -0,0 +1,24 @@
+#############################################################
+###  _____           _  ___  _ _   
+### |___ /_  ___ __ / |/ _ \(_) |_ 
+###   |_ \ \/ / '_ \| | | | | | __|
+###  ___) >  <| |_) | | |_| | | |_ 
+### |____/_/\_\ .__/|_|\___/|_|\__|
+###           |_|                  
+###                                                          
+### name: tmp8.py
+### function: sfdjlk
+### date: 2016-08-04
+### author: quanyechavshuo
+### blog: https://3xp10it.github.io
+#############################################################
+# -*- coding: utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+#upon 4 lines for chinese support
+import time
+from exp10it import *
+figlet2file("3xp10it","/tmp/figletpic",True)
+time.sleep(1)
+
