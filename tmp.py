ec3545b256d6a989ce9447f45e9709395356c7d1
3xp10it <quanyechavshuo@gmail.com>
2016-08-04 20:19:41 +0800

    test for 44

--- tmp.py
+++ tmp.py
@@ -1,16 +1,16 @@
 #############################################################
-###    _____           _______  _ __    
-###   |__  /_  ______ <  / __ \(_) /_   
-###    /_ <| |/_/ __ \/ / / / / / __/   
-###  ___/ />  </ /_/ / / /_/ / / /_     
-### /____/_/|_/ .___/_/\____/_/\__/     
-###          /_/                        
+###  _____           _  ___  _ _   
+### |___ /_  ___ __ / |/ _ \(_) |_ 
+###   |_ \ \/ / '_ \| | | | | | __|
+###  ___) >  <| |_) | | |_| | | |_ 
+### |____/_/\_\ .__/|_|\___/|_|\__|
+###           |_|                  
 ###                                                          
-### name: tmp.py                                                  
-### function: sdfjkl                                         
-### date: 2016-08-04                                         
-### author: quanyechavshuo                                   
-### blog: https://3xp10it.github.io                          
+### name: tmp.py
+### function: test for 44
+### date: 2016-08-04
+### author: quanyechavshuo
+### blog: https://3xp10it.github.io
 #############################################################
 # -*- coding: utf-8 -*-
 import sys
@@ -21,25 +21,4 @@ import time
 from exp10it import *
 figlet2file("3xp10it",2,True)
 time.sleep(1)
-
-#############################################################
-### 3xp10it             
-###                                                          
-### name: tmp.py                                                  
-### function: haode,zheci                                    
-### date: 2016-08-04                                         
-### author: quanyechavshuo                                   
-### blog: https://3xp10it.github.io                          
-#############################################################
-# -*- coding: utf-8 -*-
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
-#upon 4 lines for chinese support
-import time
-from exp10it import *
-figlet2file("3xp10it",2,True)
-time.sleep(1)
-
-this is a test
-2nd test
+66
