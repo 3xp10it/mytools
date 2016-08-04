3945bbb45d56503cc94aa22abe25f60bd72ca80b
3xp10it <quanyechavshuo@gmail.com>
2016-08-04 23:21:26 +0800

    sldfakj

---
+++ tmp3.py
@@ -0,0 +1,26 @@
+#############################################################
+###                             
+###  mm         .m   mm  '   .  
+### ' '[. . .m,  ]  .`',.m  .dm 
+###  .m` b[ ]`T  ]  ] ,[ ]   ]  
+###   '[ d, ] ]  ]  ]  [ ]   ]  
+### 'md`.`\ ]bP .dm  bd .dm  'm 
+###         ]                   
+###         '                   
+###                                                          
+### name: tmp3.py
+### function: sldfakj
+### date: 2016-08-04
+### author: quanyechavshuo
+### blog: https://3xp10it.github.io
+#############################################################
+# -*- coding: utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+#upon 4 lines for chinese support
+import time
+from exp10it import *
+figlet2file("3xp10it","/tmp/figletpic",True)
+time.sleep(1)
+jjjjjjjjjj
