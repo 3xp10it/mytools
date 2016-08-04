ac1f04d0144f59b136421f24fd2256d18ae3492d
3xp10it <quanyechavshuo@gmail.com>
2016-08-04 09:05:47 +0800

    sdfjkl

--- tmp.py
+++ tmp.py
@@ -1,4 +1,28 @@
 #############################################################
+###    _____           _______  _ __    
+###   |__  /_  ______ <  / __ \(_) /_   
+###    /_ <| |/_/ __ \/ / / / / / __/   
+###  ___/ />  </ /_/ / / /_/ / / /_     
+### /____/_/|_/ .___/_/\____/_/\__/     
+###          /_/                        
+###                                                          
+### name: tmp.py                                                  
+### function: sdfjkl                                         
+### date: 2016-08-04                                         
+### author: quanyechavshuo                                   
+### blog: https://3xp10it.github.io                          
+#############################################################
+# -*- coding: utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+#upon 4 lines for chinese support
+import time
+from exp10it import *
+figlet2file("3xp10it",2,True)
+time.sleep(1)
+
+#############################################################
 ### 3xp10it             
 ###                                                          
 ### name: tmp.py                                                  
@@ -18,3 +42,4 @@ figlet2file("3xp10it",2,True)
 time.sleep(1)
 
 this is a test
+2nd test
