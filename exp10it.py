9c0e18b448208930a7cfddcb38e36e4baf0e3215
3xp10it <quanyechavshuo@gmail.com>
2016-08-05 22:01:38 +0800

    my latest python module

--- exp10it.py
+++ exp10it.py
@@ -17,7 +17,6 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 #upon 4 lines for chinese support
 
-
 import os
 import re
 import random
@@ -246,3 +245,53 @@ readline.parse_and_bind("tab: complete")
 readline.set_completer(t.pathCompleter)
 #--------------------------end-of-key-complete-with-file-path--------------------------------
 
+def get_title_from_uri(uri):
+    #得到uri对应的title
+    import mechanize
+    import cookielib
+    br = mechanize.Browser()
+    br.set_cookiejar(cookielib.LWPCookieJar()) # Cookie jar
+    br.set_handle_equiv(True) # Browser Option
+    br.set_handle_gzip(True)
+    br.set_handle_redirect(True)
+    br.set_handle_referer(True)
+    br.set_handle_robots(False)
+    br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+    br.open(uri)
+    #print br.title()
+    return br.title()
+
+def get_uris_from_file(file):
+    #从文件中获取所有uri
+    f=open(file,"r+")
+    content=f.read()
+    #print content
+    f.close()
+    allurls=[]
+    all=re.findall('(http(\S)+)',content,re.I)
+    for each in all:
+        allurls.append(each[0])
+    #print allurls
+    return allurls
+
+def get_title_from_file(file):
+    #等到文件中的所有uri对应的title
+    target_allurls=get_uris_from_file(file)
+    print "a output file:/tmp/result.txt"
+    writed_urls=[]
+    for each in target_allurls:
+        f=open("/tmp/result.txt","a+")
+        tmp=urlparse(each)
+        http_domain=tmp.scheme+'://'+tmp.hostname
+        title=get_title_from_uri(http_domain)
+        time.sleep(1)
+        try:
+            if http_domain not in writed_urls:
+                each_line_to_write=http_domain+'\r\n'+'upon uri is:'+title+'\r\n'
+                print each_line_to_write
+                f.write(each_line_to_write)
+                writed_urls.append(http_domain)
+        except:
+            pass
+    f.close()
