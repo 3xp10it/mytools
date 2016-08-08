684b27bcb5cfa14f99c8dc66d3e3171a10a57343
3xp10it <quanyechavshuo@gmail.com>
2016-08-08 08:18:59 +0800

    write blog

--- blog.py
+++ blog.py
@@ -1,3 +1,27 @@
+#############################################################
+###                                                                                                                                                                                      _   _  ___  _           _____ 
+###                                                                                                                                                                                     | |_(_)/ _ \/ |_ ____  _|___ / 
+###                                                                                                                                                                                     | __| | | | | | '_ \ \/ / |_ \ 
+###                                                                                                                                                                                     | |_| | |_| | | |_) >  < ___) |
+###                                                                                                                                                                                      \__|_|\___/|_| .__/_/\_\____/ 
+###                                                                                                                                                                                                   |_|              
+###                                                          
+### name: blog.py
+### function: write blog
+### date: 2016-08-08
+### author: quanyechavshuo
+### blog: https://3xp10it.github.io
+#############################################################
+# -*- coding: utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+#upon 4 lines for chinese support
+import time
+from exp10it import *
+figlet2file("3xp10it","/tmp/figletpic",True)
+time.sleep(1)
+
 # -*- coding: utf-8 -*-
 import sys
 reload(sys)
