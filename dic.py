7d90329fbe50cc14d592e42a870d1bea3f444110
3xp10it <quanyechavshuo@gmail.com>
2016-10-07 10:49:21 +0800

    clean dict

---
+++ dic.py
@@ -0,0 +1,52 @@
+#############################################################
+###                                                  
+###   mmmm                mmm     mmmm    "      m   
+###  "   "# m   m  mmmm     #    m"  "m mmm    mm#mm 
+###    mmm"  #m#   #" "#    #    #  m #   #      #   
+###      "#  m#m   #   #    #    #    #   #      #   
+###  "mmm#" m" "m  ##m#"  mm#mm   #mm#  mm#mm    "mm 
+###                #                                 
+###                "                                 
+###                                                          
+### name: dic.py
+### function: clean dict
+### date: 2016-10-07
+### author: quanyechavshuo
+### blog: https://3xp10it.github.io
+#############################################################
+# -*- coding: utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+#upon 4 lines for chinese support
+import time
+from exp10it import *
+figlet2file("3xp10it","/tmp/figletpic",True)
+time.sleep(1)
+
+import sys
+import re
+def usage():
+    print "example:%s -f file.txt" % sys.argv[0]
+    print "there will be a newfile.txt in the same directory."
+    sys.exit(0)
+def main():
+    if sys.argv[1]!='-f':
+    	usage()
+    file=open(sys.argv[2],"r+")
+    list=file.readlines()
+    file.close()
+    
+    
+    for each in list:
+        each=re.sub(r"(\s)$","",each)
+    	newfile=open("newfile.txt","a+")
+    	new_list=newfile.readlines()
+    	if each+'\r\n' not in new_list:
+    		newfile.write(each+'\r\n')
+    	newfile.flush()
+    	newfile.close()
+
+
+if __name__=="__main__":
+    main()
