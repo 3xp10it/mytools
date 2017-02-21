625568a9be9308388f8d3fbbb65c485920b1c710
3xp10it <3xp10it@users.noreply.github.com>
2017-02-21 17:19:45 +0800

    Update dic.py

--- dic.py
+++ dic.py
@@ -14,11 +14,6 @@
 ### author: quanyechavshuo
 ### blog: https://3xp10it.github.io
 #############################################################
-# -*- coding: utf-8 -*-
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
-#upon 4 lines for chinese support
 import time
 from exp10it import *
 figlet2file("3xp10it","/tmp/figletpic",True)
@@ -27,8 +22,8 @@ time.sleep(1)
 import sys
 import re
 def usage():
-    print "example:%s -f file.txt" % sys.argv[0]
-    print "there will be a newfile.txt in the same directory."
+    print("example:%s -f file.txt" % sys.argv[0])
+    print("there will be a newfile.txt in the same directory.")
     sys.exit(0)
 def main():
     if sys.argv[1]!='-f':
