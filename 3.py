08b6877a524dd655e7ed080feb42fedd08f75b0b
3xp10it <quanyechavshuo@gmail.com>
2017-03-19 18:20:27 +0800

    update

--- 3.py
+++
@@ -1,7 +0,0 @@
-import requests
-import re
-html=requests.get("https://github.com/macvim-dev/macvim/releases/").text
-a=re.findall(r'''href="(.*\.dmg)"''',html,re.I)
-macvimUrl="https://github.com"+a[0]
-print(macvimUrl)
-
