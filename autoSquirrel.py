0b838ccaa6cd4ee27543e879c1375f2c0fbd7f6b
3xp10it <quanyechavshuo@gmail.com>
2017-05-20 12:48:13 +0800

    AutoIm or autoSquirrel

---
+++ autoSquirrel.py
@@ -0,0 +1,8 @@
+import os
+command='''
+sudo -u Guest brew cask install squirrel
+rm ~/Library/Rime/default.custom.yaml
+wget https://gist.githubusercontent.com/3xp10it/9f51ba895e47cdd5ed1b04f765298df9/raw/8d25bbcc5483e9f77ba24a3a5e84d28cc937fd9a/default.custom.yaml -O ~/Library/Rime/default.custom.yaml
+'''
+os.system(command)
+print("请重新部署squirrel使设置生效")
