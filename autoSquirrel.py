eefe9d4be2ba4afdf6467a2bd9425a70a5f02f06
3xp10it <quanyechavshuo@gmail.com>
2017-05-20 13:44:38 +0800

    update

--- autoSquirrel.py
+++ autoSquirrel.py
@@ -1,6 +1,6 @@
 import os
 command='''
-sudo -u Guest brew cask install squirrel
+brew cask install squirrel
 rm ~/Library/Rime/default.custom.yaml
 wget https://gist.githubusercontent.com/3xp10it/9f51ba895e47cdd5ed1b04f765298df9/raw/8d25bbcc5483e9f77ba24a3a5e84d28cc937fd9a/default.custom.yaml -O ~/Library/Rime/default.custom.yaml
 '''
