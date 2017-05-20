ad6c0343b1484d46da708fda4a5a5a352d21a3f6
3xp10it <quanyechavshuo@gmail.com>
2017-05-20 14:07:00 +0800

    upa

--- autoSquirrel.py
+++ autoSquirrel.py
@@ -4,7 +4,7 @@ brew cask install squirrel
 rm ~/Library/Rime/default.custom.yaml
 '''
 os.system(command)
-print("Attention!!!\nPlease restart your squirrel or just make your setting take effect")
+input("Attention!!!\nPlease restart your squirrel or just make your setting take effect \nAnd press any key after you finish it")
 command="wget https://gist.githubusercontent.com/3xp10it/9f51ba895e47cdd5ed1b04f765298df9/raw/8d25bbcc5483e9f77ba24a3a5e84d28cc937fd9a/default.custom.yaml -O ~/Library/Rime/default.custom.yaml"
 os.system(command)
-print("Attention!!!\nPlease restart your squirrel or just make your setting take effect")
+input("Attention!!!\nPlease restart your squirrel or just make your setting take effect \nAnd press any key after you finish it")
