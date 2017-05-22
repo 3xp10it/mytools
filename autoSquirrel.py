039df0339ccde90510d71c9e5c7c4c365fcdf4f2
3xp10it <quanyechavshuo@gmail.com>
2017-05-22 20:45:18 +0800

    up

--- autoSquirrel.py
+++ autoSquirrel.py
@@ -5,6 +5,6 @@ rm ~/Library/Rime/default.custom.yaml
 '''
 os.system(command)
 input("Attention!!!\nPlease do:\n1.add squirrel as your input method \n2.set up your squirrel as you like with this tool:\nhttps://github.com/neolee/SCU\n3.restart your squirrel or just make your setting take effect \n4.press any key after you finish it...")
-command="wget https://gist.githubusercontent.com/3xp10it/9f51ba895e47cdd5ed1b04f765298df9/raw/8d25bbcc5483e9f77ba24a3a5e84d28cc937fd9a/default.custom.yaml -O ~/Library/Rime/default.custom.yaml"
+command="wget https://gist.githubusercontent.com/3xp10it/9f51ba895e47cdd5ed1b04f765298df9/raw/f5f8504fb831fa23f8d80eb34ac97491d6577d93/default.custom.yaml -O ~/Library/Rime/default.custom.yaml"
 os.system(command)
 input("Attention!!!\nPlease do:\n1.restart your squirrel or just make your setting take effect \n2.press any key after you finish it...")
