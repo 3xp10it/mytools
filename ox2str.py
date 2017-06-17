bb20b532ee70e0de5a3b08e86c8735ef13317553
3xp10it <quanyechavshuo@gmail.com>
2017-06-17 10:21:47 +0800

    up

---
+++ ox2str.py
@@ -0,0 +1,23 @@
+import re
+from exp10it import post_request
+while 1:
+    postUrl="http://javatools.bejson.com/tools/convert/hexChar"
+    ox=input("Please input your string from hopper:>")
+    if re.match(r"\\[^\\]+",ox):
+        #只有一个\
+        print("1个")
+    if re.match(r"\\{2}",ox):
+        #有两个\
+        print("2个")
+        ox=re.sub(r"\\{2}",r"\\",ox)
+        #print(ox)
+    data={}
+    data['oldStr']=ox
+    data['fmtType']='c'
+    req=post_request(postUrl,data)
+    result=re.search(r'''message('|"):('|")(.*?)('|")''',req,re.I).group(3)
+    print(result)
+
+
+    
+
