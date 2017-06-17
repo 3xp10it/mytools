d249b2a2bb97c0cd364894134e382aaf5ab0a2b6
3xp10it <quanyechavshuo@gmail.com>
2017-06-17 10:27:58 +0800

    up

--- ox2str.py
+++ ox2str.py
@@ -3,23 +3,17 @@ import os
 os.system("pip3 install exp10it -U --no-cache")
 from exp10it import post_request
 while 1:
-    postUrl="http://javatools.bejson.com/tools/convert/hexChar"
-    ox=input("Please input your string from hopper:>")
-    if re.match(r"\\[^\\]+",ox):
-        #只有一个\
-        #print("1个")
-    if re.match(r"\\{2}",ox):
-        #有两个\
-        #print("2个")
-        ox=re.sub(r"\\{2}",r"\\",ox)
-        #print(ox)
-    data={}
-    data['oldStr']=ox
-    data['fmtType']='c'
-    req=post_request(postUrl,data)
-    result=re.search(r'''message('|"):('|")(.*?)('|")''',req,re.I).group(3)
+    postUrl = "http://javatools.bejson.com/tools/convert/hexChar"
+    ox = input("Please input your string from hopper:>")
+    if re.match(r"\\[^\\]+", ox):
+        # 只有一个\符号
+        pass
+    if re.match(r"\\{2}", ox):
+        # 有两个\符号
+        ox = re.sub(r"\\{2}", r"\\", ox)
+    data = {}
+    data['oldStr'] = ox
+    data['fmtType'] = 'c'
+    req = post_request(postUrl, data)
+    result = re.search(r'''message('|"):('|")(.*?)('|")''', req, re.I).group(3)
     print(result)
-
-
-    
-
