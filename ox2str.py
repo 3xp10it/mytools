7aee869ddc39b7e5ccf4b676bec9f0016b33bc88
3xp10it <quanyechavshuo@gmail.com>
2017-06-17 10:25:05 +0800

    up

--- ox2str.py
+++ ox2str.py
@@ -1,14 +1,16 @@
 import re
+import os
+os.system("pip3 install exp10it -U --no-cache")
 from exp10it import post_request
 while 1:
     postUrl="http://javatools.bejson.com/tools/convert/hexChar"
     ox=input("Please input your string from hopper:>")
     if re.match(r"\\[^\\]+",ox):
         #只有一个\
-        print("1个")
+        #print("1个")
     if re.match(r"\\{2}",ox):
         #有两个\
-        print("2个")
+        #print("2个")
         ox=re.sub(r"\\{2}",r"\\",ox)
         #print(ox)
     data={}
