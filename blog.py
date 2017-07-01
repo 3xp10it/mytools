856bc3231bcc23cecaf5158b9de57db52ba2daac
3xp10it <quanyechavshuo@gmail.com>
2017-07-01 08:48:17 +0800

    up

--- blog.py
+++ blog.py
@@ -15,8 +15,10 @@
 ### blog: http://3xp10it.cc
 #############################################################
 import time
+import os
 from exp10it import figlet2file
 from exp10it import blog
+os.system("pip3 install exp10it -U --no-cache")
 figlet2file("3xp10it",0,True)
 time.sleep(1)
 blog()
