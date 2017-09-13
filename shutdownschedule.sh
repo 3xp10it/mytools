a2939667abc0f4d478637d81e3de58aa6bac6290
3xp10it <quanyechavshuo@gmail.com>
2017-09-13 23:25:20 +0800

    up

--- shutdownschedule.sh
+++ shutdownschedule.sh
@@ -1,7 +1,1 @@
-#!/usr/bin/env python3
-import os
-from datetime import datetime
-weekIndex = datetime.now().weekday()
-if weekIndex not in [5, 6]:
-    os.system("say cool boy,还有1分钟就要关机了")
-    os.system("shutdown -h +1")
+python3 /usr/share/mytools/shutdownschedule.py
