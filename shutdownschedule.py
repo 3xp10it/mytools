7ed9acaee99729c8b05fd23b23b6b336b2405d98
3xp10it <quanyechavshuo@gmail.com>
2017-09-18 23:42:54 +0800

    up

--- shutdownschedule.py
+++ shutdownschedule.py
@@ -2,6 +2,6 @@
 import os
 from datetime import datetime
 weekIndex = datetime.now().weekday()
-if weekIndex not in [5, 6]:
+if weekIndex not in [4,5, 6]:
     os.system("say cool boy,还有1分钟就要关机了")
     os.system("shutdown -h +1")
