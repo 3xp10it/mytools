ab0b1f3e3493fbd4521f6eb16eee2a71544fec6e
3xp10it <quanyechavshuo@gmail.com>
2017-10-20 12:21:26 +0800

    up

--- up.py
+++ up.py
@@ -6,7 +6,7 @@ if os.path.exists(homePath+'/myblog') is False and os.path.exists(homePath+'/3xp
     print "this is the first time you use me,I will download 3xp10it.github.io from https://github.com/3xp10it/3xp10it.github.io.git"
     os.system("mkdir ~/3xp10it.github.io && cd ~/3xp10it.github.io && git init && git pull https://github.com/3xp10it/3xp10it.github.io.git && git remote add origin https://github.com/3xp10it/3xp10it.github.io.git")
 else:
-    os.system('''cd ~/myblog && kill -9 $(ps -a | egrep "jekyll" | egrep -o "\d+" | head -n 1);sleep 3 && cp index.html tmp.html && cp index_bak.html index.html && sh -c "rm -r _site/ & jekyll serve --incremental --watch && exit" && cp _site/index.html index2.html && cp tmp.html index.html && echo congratulations! commands execute ok to here!''')
-    os.system('''cd ~/3xp10it.github.io && cp -r ../myblog/_site/* . && cp ../myblog/index2.html . && cp ../myblog/index.html . && git status && git add . && git status && git commit -a -m "update" && git push -u origin master && echo congratulations! commands execute ok to here!''')
-    os.system('''cd ~/myblog && git status && git add . && git status && git commit -a -m "update backup" && git push -u origin master && echo all is well,and all is done!''')
+    os.system('''cd ~/myblog && git pull && kill -9 $(ps -a | egrep "jekyll" | egrep -o "\d+" | head -n 1);sleep 3 && cp index.html tmp.html && cp index_bak.html index.html && sh -c "rm -r _site/ & jekyll serve --incremental --watch && exit" && cp _site/index.html index2.html && cp tmp.html index.html && echo congratulations! commands execute ok to here!''')
+    os.system('''cd ~/3xp10it.github.io && git pull && cp -r ../myblog/_site/* . && cp ../myblog/index2.html . && cp ../myblog/index.html . && git status && git add . && git commit -a -m "update" && git push -u origin master && echo congratulations! commands execute ok to here!''')
+    os.system('''cd ~/myblog && git pull && git status && git add . && git commit -a -m "update backup" && git push -u origin master && echo all is well,and all is done!''')
 
--- up.sh
+++ up.sh
@@ -1,8 +1,8 @@
 if [ -d ~/myblog ] && [ -d ~/3xp10it.github.io ]
 then
-    cd ~/myblog && kill -9 $(ps -a | egrep "jekyll" | egrep -o "\d+" | head -n 1);sleep 3 && cp index.html tmp.html && cp index_bak.html index.html && sh -c "rm -r _site/ & jekyll serve --incremental --watch && exit" && cp _site/index.html index2.html && cp tmp.html index.html && echo congratulations! commands execute ok to here!
-    cd ~/3xp10it.github.io && cp -r ../myblog/_site/* . && cp ../myblog/index2.html . && cp ../myblog/index.html . && git status && git add . && git status && git commit -a -m "update" && git push -u origin master && echo congratulations! commands execute ok to here!
-    cd ~/myblog && git status && git add . && git status && git commit -a -m "update backup" && git push -u origin master && echo all is well,and all is done!
+    cd ~/myblog && git pull && kill -9 $(ps -a | egrep "jekyll" | egrep -o "\d+" | head -n 1);sleep 3 && cp index.html tmp.html && cp index_bak.html index.html && sh -c "rm -r _site/ & jekyll serve --incremental --watch && exit" && cp _site/index.html index2.html && cp tmp.html index.html && echo congratulations! commands execute ok to here!
+    cd ~/3xp10it.github.io && git pull && cp -r ../myblog/_site/* . && cp ../myblog/index2.html . && cp ../myblog/index.html . && git status && git add . && git commit -a -m "update" && git push -u origin master && echo congratulations! commands execute ok to here!
+    cd ~/myblog && git pull && git status && git add . && git commit -a -m "update backup" && git push -u origin master && echo all is well,and all is done!
 else
     echo "this is the first time you use me,I will download myblog from https://github.com/3xp10it/myblog.git"
     mkdir ~/myblog && cd ~/myblog && git init && git pull https://github.com/3xp10it/myblog.git && git remote add origin https://github.com/3xp10it/myblog.git
