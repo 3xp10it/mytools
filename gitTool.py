5e2c9ea9a9f1959a778b60decbf8a571e4fb6a33
3xp10it <quanyechavshuo@gmail.com>
2017-10-21 14:30:26 +0800

    up

---
+++ gitTool.py
@@ -0,0 +1,14 @@
+# 每天第1次接触电脑时运行py3 gitTool.py --update
+# 每天第-1次接触电脑时运行py3 gitTool.py --commit
+
+import sys
+import os
+dirList = ["~/myblog/", "~/3xp10it.github.io/",
+           "~/mypypi/", "/usr/share/mytools"]
+if sys.argv[1] == "--update":
+    for eachDir in dirList:
+        os.system("cd %s && git pull" % eachDir)
+if sys.argv[1] == "--commit":
+    for eachDir in dirList:
+        os.system(
+            "cd %s && git add . && git commit -a -m 'up' && git push -u origin master" % eachDir)
