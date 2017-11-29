e59b73dfa79d31be997d0e51708475df57584224
3xp10it <quanyechavshuo@gmail.com>
2017-11-29 18:15:02 +0800

    up

---
+++ xxx.py
@@ -0,0 +1,32 @@
+#使用说明
+#python3 xxx.py package.txt targets.txt 'secret'
+#python3 xxx.py 包文件 目标列表文件 返回内容中存在漏洞的正则表达式
+import re
+from concurrent import futures
+from exp10it import send_http_package
+from urllib.parse import urlparse
+import sys
+package_file=sys.argv[1]
+targets_file=sys.argv[2]
+pattern_string=sys.argv[3]
+pattern=re.compile(pattern_string,re.I)
+
+with open(package_file,"r+") as f:
+    package=f.read()
+with open(targets_file,"r+") as f:
+    targets=f.readlines()
+
+def send_single_package(each):
+    global package
+    each=re.sub(r"\s$","",each)
+    parsed=urlparse(each)
+    host=parsed.netloc
+    each_package=re.sub(r"(?<=Host: )(.+)",host,package,re.I)
+    each_package=re.sub(r"(?<=Referer: )(.+)",each,each_package,re.I)
+    a=send_http_package(each_package,each.split(":")[0])
+    if pattern.search(a):
+        print("%s 漏洞存在" % each)
+
+with futures.ThreadPoolExecutor(max_workers=10) as executor:
+    executor.map(send_single_package,targets)
+
