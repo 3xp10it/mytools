00618d8fd5541a6cb216111f250c22bcab866f7b
3xp10it <quanyechavshuo@gmail.com>
2018-01-08 18:11:26 +0800

    up

--- record.js
+++ record.js
@@ -9,12 +9,16 @@ if (ObjC.available)
         console.log("[*] Method Name: " + funcName);
         Interceptor.attach(hook.implementation, {
           onEnter: function(args) {
-            console.log("param:"+args[0]+" type:"+typeof args[0]);
+            //注意:有时args参数值在这里会是一个对象,如果函数返回值是字符串类型,为了更好理解则要这样写
+            //这里假设args[2]是要记录的参数
+            //ObjC.classes.NSString.stringWithString_(args[2])或者args[2].toString()或者ObjC.classes.NSString.stringWithString_(args[2]).toString()
+            //具体情况需要测试下是上面这3种的哪种写法
+            console.log("param:"+args[2]+" type:"+typeof args[2]);
           },
           onLeave: function(retval) {
-            //注意:retval永远是一个对象,如果函数返回值是字符串类型,为了更好理解则要这样写
-            //string_value=ObjC.classes.NSString.stringWithString_(retval)
-            //console.log("Return value-> (type:"+typeof string_value+",value:"+string_value+")");
+            //注意:retval一般会返回一个对象,如果函数返回值是字符串类型,为了更好理解则要这样写
+            //ObjC.classes.NSString.stringWithString_(retval)或者retval.toString()或者ObjC.classes.NSString.stringWithString_(retval).toString()
+            //具体情况需要测试下是上面这3种的哪种写法
             console.log("Return value-> (type:"+typeof retval+",value:"+retval+")");
           }
         });
