658757a92cdf11879e8222f780782b82c4b431f8
3xp10it <quanyechavshuo@gmail.com>
2018-06-05 17:51:51 +0800

    up

--- password.py
+++ password.py
@@ -6,7 +6,7 @@ filename = _['file']
 password = _['pass']
 os.system("openssl aes-256-cbc -d -in %s -out ~/.password_tmp_file -k '%s'" %
           (filename, password))
-os.system("/Applications/MacVim.app/Contents/MacOS/Vim ~/.password_tmp_file")
+os.system("vim  ~/.password_tmp_file")
 os.system("openssl aes-256-cbc -in ~/.password_tmp_file -out %s -k '%s'" %
           (filename, password))
 os.system("rm ~/.password_tmp_file")
