23e4c9dccb99edac470d4850bf3912ce78a128e1
3xp10it <quanyechavshuo@gmail.com>
2018-06-05 17:31:40 +0800

    up

--- mysnippingtool.py
+++ mysnippingtool.py
@@ -2,7 +2,7 @@
 #python3 mysnippingtool
 #python3 mysnippingtool -ua
 #python3 mysnippingtool -co
-#如果删除了~/githubpic目录下的图片,则要运行python3 mysnippingtool -ua表示手动更新address.txt中的列表
+#如果删除了~/pic目录下的图片,则要运行python3 mysnippingtool -ua表示手动更新address.txt中的列表
 #这种情况使用在人工测试mysnippintool脚本是否有错的时候会偶尔用到
 
 #-co表示打印出需要组合图片时候要访问的网站
@@ -45,17 +45,17 @@ def get_all_file_name(folder):
 def main():
     import sys
     home_path=get_home_path()
-    githubpic_path=home_path+"/githubpic"
+    pic_path=home_path+"/pic"
 
-    if os.path.exists(githubpic_path) is False:
-        print("this is the first time you use me,or you have deleted ~/githubpic,I will mkdir ~/githubpic and git pull the github's pic.git,please put pngs to ~/githubpic when needed,and don't delet any png file in this folder")
-        os.system("mkdir ~/githubpic && cd ~/githubpic && git init && git pull https://github.com/3xp10it/pic.git && git remote add origin https://github.com/3xp10it/pic.git && git status")
+    if os.path.exists(pic_path) is False:
+        print("this is the first time you use me,or you have deleted ~/pic,I will mkdir ~/pic and git pull the github's pic.git,please put pngs to ~/pic when needed,and don't delet any png file in this folder")
+        os.system("mkdir ~/pic && cd ~/pic && git init && git pull https://github.com/3xp10it/pic.git && git remote add origin https://github.com/3xp10it/pic.git && git status")
         return
 
-    address_path=githubpic_path+"/address.txt"
+    address_path=pic_path+"/address.txt"
 
     if len(sys.argv)==2 and sys.argv[1]=="-ua":
-        all_png_list=get_all_file_name(githubpic_path)
+        all_png_list=get_all_file_name(pic_path)
 
         os.system("rm %s" % address_path)
         with open(address_path,"a+") as f:
@@ -74,9 +74,9 @@ def main():
         os.system("firefox %s" % on_line_addr)
         return
 
-    os.system("cd ~/githubpic && git add . && git status && git commit -a -m 'update' && git push -u origin master")
+    os.system("cd ~/pic && git add . && git status && git commit -a -m 'update' && git push -u origin master")
 
-    all_png_list=get_all_file_name(githubpic_path)
+    all_png_list=get_all_file_name(pic_path)
     #print(all_png_list)
 
 
@@ -93,7 +93,7 @@ def main():
                         f.write(each_addr+'\r\n')
                         f.close()
         except:
-            print("open ~/githubpic/address.txt wrong")
+            print("open ~/pic/address.txt wrong")
 
 if __name__=='__main__':
     main()
@@ -105,9 +105,9 @@ print os.path.abspath(each) is not good function,
 it will get ~/桌面/spider_wooyun when I put this py script file in ~/桌面,and run:
 (cd 桌面)
 python mysnippingtool.py
-it will get "~/桌面/spider_wooyun" as a result,but the true result should be "~/githubpic/spider_wooyun"
+it will get "~/桌面/spider_wooyun" as a result,but the true result should be "~/pic/spider_wooyun"
 so I use:
-current_file_abspath=os.path.isdir("~/githubpic/"+each):
+current_file_abspath=os.path.isdir("~/pic/"+each):
 to get the current file's abspath
 '''
 
