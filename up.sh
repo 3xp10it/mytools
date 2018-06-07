0990fa56eec2120ac3c7f0abad53fc11b0bf8c58
3xp10it <quanyechavshuo@gmail.com>
2018-06-07 08:18:18 -0400

    up

--- up.sh
+++ up.sh
@@ -1,6 +1,6 @@
 if [ -d ~/myblog ] && [ -d ~/3xp10it.github.io ]
 then
-    cd ~/myblog && cp index.html tmp.html && cp index_bak.html index.html && sh -c "rm -r _site/ & jekyll serve --incremental --watch && exit" && cp _site/index.html index2.html && cp tmp.html index.html && echo congratulations! generate new _site content finished.
+    cd ~/myblog && cp index.html tmp.html && cp index_bak.html index.html && rm -r _site/ && jekyll serve --incremental --watch && cp _site/index.html index2.html && cp tmp.html index.html && echo congratulations! generate new _site content finished.
     cd ~/3xp10it.github.io && cp -r ../myblog/_site/* . && cp ../myblog/index2.html . && cp ../myblog/index.html . && git status && git add . && git commit -a -m "update" && git push -u origin master && echo congratulations! upload 3xp10it.github.io finished.
     cd ~/myblog && git status && git add . && git commit -a -m "update backup" && git push -u origin master && echo upload myblog finished.
 else
