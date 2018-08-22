af1ab5ebf3f0cba8a5963df75007c85541cab728
3xp10it <quanyechavshuo@gmail.com>
2018-08-22 04:52:50 -0400

    up

--- password.py
+++ password.py
@@ -1,5 +1,12 @@
 import os
+import sys
 import platform
+
+system=platform.system()
+if system!="Darwin":
+    print("You can only use macOS handle it currently.")
+    sys.exit(0)
+
 config = os.path.expanduser("~") + "/.password"
 with open(config, "r+") as f:
     _ = eval(f.read())
@@ -11,6 +18,7 @@ system=platform.system()
 if system=="Darwin":
     cmd="/Applications/MacVim.app/Contents/MacOS/Vim ~/.password_tmp_file"
 else:
+    sys.exit(0)
     cmd="vim  ~/.password_tmp_file"
 os.system(cmd)
 os.system("openssl aes-256-cbc -in ~/.password_tmp_file -out %s -k '%s'" %
