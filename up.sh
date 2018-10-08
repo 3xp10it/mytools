180095a9d69ef2a0ee8a24162e5d7f06633e5fc6
3xp10it <quanyechavshuo@gmail.com>
2018-10-08 04:56:55 -0400

    up

--- up.sh
+++ up.sh
@@ -1,8 +1,8 @@
 if [ -d ~/myblog ] && [ -d ~/3xp10it.github.io ]
 then
-    cd ~/myblog && cp index.html tmp.html && cp index_bak.html index.html && rm -r _site/ && jekyll serve --incremental --watch && cp _site/index.html index2.html && cp tmp.html index.html && echo congratulations! generate new _site content finished.
-    cd ~/3xp10it.github.io && cp -r ../myblog/_site/* . && cp ../myblog/index2.html . && cp ../myblog/index.html . && git status && git add . && git commit -a -m "update" && git push -u origin master && echo congratulations! upload 3xp10it.github.io finished.
-    cd ~/myblog && git status && git add . && git commit -a -m "update backup" && git push -u origin master && echo upload myblog finished.
+    cd ~/myblog && cp index.html tmp.html && cp index_bak.html index.html && rm -r _site/ && jekyll serve --incremental --watch && cp _site/index.html index2.html && cp tmp.html index.html && echo -e "\033[31m congratulations! generate new _site content finished. \033[0m"
+    cd ~/3xp10it.github.io && cp -r ../myblog/_site/* . && cp ../myblog/index2.html . && cp ../myblog/index.html . && git status && git add . && git commit -a -m "update" && git push -u origin master && echo -e "\033[31m congratulations! upload 3xp10it.github.io finished. \033[0m"
+    cd ~/myblog && git status && git add . && git commit -a -m "update backup" && git push -u origin master && echo -e "\033[31m upload myblog finished. \033[0m"
 else
     echo "this is the first time you use me,I will download myblog from https://github.com/3xp10it/myblog.git"
     mkdir ~/myblog && cd ~/myblog && git init && git pull https://github.com/3xp10it/myblog.git && git remote add origin https://github.com/3xp10it/myblog.git
