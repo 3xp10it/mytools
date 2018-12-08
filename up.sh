f0fc5a3d353a4af1ed69567548a97eeaf8de14ba
3xp10it <quanyechavshuo@gmail.com>
2018-12-08 21:17:27 +0800

    up

--- up.sh
+++ up.sh
@@ -1,7 +1,7 @@
 if [ -d ~/myblog ] && [ -d ~/3xp10it.github.io ]
 then
     cd ~/myblog && cp index.html tmp.html && cp index_bak.html index.html && rm -r _site/ && jekyll serve --incremental --watch && cp _site/index.html index2.html && cp tmp.html index.html && echo -e "\033[31m congratulations! generate new _site content finished. \033[0m"
-    cd ~/3xp10it.github.io && cp -r ../myblog/_site/* . && cp ../myblog/index2.html . && cp ../myblog/index.html . && git status && git add . && git commit -a -m "update" && git push -u origin master && echo -e "\033[31m congratulations! upload 3xp10it.github.io finished. \033[0m"
+    cd ~/3xp10it.github.io && rm -r * && cp -r ../myblog/_site/* . && cp ../myblog/index2.html . && cp ../myblog/index.html . && git status && git add . && git commit -a -m "update" && git push -u origin master && echo -e "\033[31m congratulations! upload 3xp10it.github.io finished. \033[0m"
     cd ~/myblog && git status && git add . && git commit -a -m "update backup" && git push -u origin master && echo -e "\033[31m upload myblog finished. \033[0m"
 else
     echo "this is the first time you use me,I will download myblog from https://github.com/3xp10it/myblog.git"
