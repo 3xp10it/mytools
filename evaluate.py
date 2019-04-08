4b4e8d02cb126f6c7af050bfa503680df341eeb8
3xp10it <quanyechavshuo@gmail.com>
2019-04-08 18:39:54 +0800

    up

---
+++ evaluate.py
@@ -0,0 +1,10 @@
+choose=input("buy or sell? default [1]\n1.buy\n2.sell\n:>")
+high=float(input("please input high price:\n"))
+low=float(input("please input low price:\n"))
+if choose=='1':
+    buy=float(input("please input your buy price:\n"))
+    score=(high-buy)/(high-low)
+elif choose=='2':
+    sell=float(input("please input your sell price:\n"))
+    score=(sell-low)/(high-low)
+print(round(score))
