a960add97683af564925155f77f295ec43be4671
3xp10it <quanyechavshuo@gmail.com>
2019-05-19 00:29:14 +0800

    up

--- evaluate.py
+++ evaluate.py
@@ -1,11 +1,13 @@
 import pdb
 choose=input("buy or sell? default [1]\n1.buy\n2.sell\n:>")
-high=float(input("please input high price:\n"))
-low=float(input("please input low price:\n"))
 if choose=='2':
     sell=float(input("please input your sell price:\n"))
+    high=float(input("please input high price:\n"))
+    low=float(input("please input low price:\n"))
     score=(sell-low)/(high-low)*100
 else:
     buy=float(input("please input your buy price:\n"))
+    high=float(input("please input high price:\n"))
+    low=float(input("please input low price:\n"))
     score=(high-buy)/(high-low)*100
 print("得分:"+str(round(score)))
