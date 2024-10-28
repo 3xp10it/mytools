bc6dc59b7393bf251eec1666e8615d7330777721
3xp10it <quanyechavshuo@gmail.com>
2024-10-28 12:41:12 +0800

    up

--- up.sh
+++ up.sh
@@ -9,7 +9,8 @@ then
     cd ~/myblog && git status && git add . && git commit -a -m "update backup" && git push -u origin master && echo -e "\033[31m upload myblog finished. \033[0m"
 else
     echo "this is the first time you use me,I will download myblog from https://github.com/3xp10it/myblog.git"
-    mkdir ~/myblog && cd ~/myblog && git init && git pull https://github.com/3xp10it/myblog.git && git remote add origin https://github.com/3xp10it/myblog.git
+    #mkdir ~/myblog && cd ~/myblog && git init && git pull https://github.com/3xp10it/myblog.git && git remote add origin https://github.com/3xp10it/myblog.git
+    cd && git clone https://github.com/3xp10it/myblog.git 
     echo "this is the first time you use me,I will download 3xp10it.github.io from https://github.com/3xp10it/3xp10it.github.io.git"
     #mkdir ~/3xp10it.github.io && cd ~/3xp10it.github.io && git init && git pull https://github.com/3xp10it/3xp10it.github.io.git && git remote add origin https://github.com/3xp10it/3xp10it.github.io.git
     cd && git clone https://github.com/3xp10it/3xp10it.github.io.git 
